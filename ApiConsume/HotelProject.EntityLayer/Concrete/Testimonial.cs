d10c349dfaaebffa2a523b17099c61c0a5e2cd9a
Burak Filiz <dev.burakfiliz@gmail.com>
2023-03-26 20:44:34 +0300

    Added Controllers

--- ApiConsume/HotelProject.EntityLayer/Concrete/Testimonial.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Testimonial.cs
@@ -9,6 +9,7 @@ namespace HotelProject.EntityLayer.Concrete
     public class Testimonial
     {
         public int TestimonialID { get; set; }
+        public string Title { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
