dfc691599a2469cc9f09f90162fe60ffe444b2c0
Burak Filiz <dev.burakfiliz@gmail.com>
2023-03-27 12:09:20 +0300

    Added Auto Mapper

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class UpdateRoomDto
+    {
+        [Required(ErrorMessage = "Lütfen ID yazınız")]
+        public int RoomID { get; set; }
+        [Required(ErrorMessage = "Lütfen oda numarası yazınız")]
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Lütfen oda görseli giriniz")]
+        public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen Fiyat bilgisi giriniz")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığı bilgisi giriniz")]
+        [StringLength(100,ErrorMessage ="Lürfen 100 karakterden fazla veri girişi yapmayınız")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısı bilgisi giriniz")]
+        public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısı bilgisi giriniz")]
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        [Required(ErrorMessage = "Lütfen açıklama giriniz")]
+        public string Description { get; set; }
+    }
+}
