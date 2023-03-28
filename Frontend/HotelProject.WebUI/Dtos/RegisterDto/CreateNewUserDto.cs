a70ead051de4049efe13d5304499cd94099790d2
Burak Filiz <dev.burakfiliz@gmail.com>
2023-03-28 11:01:42 +0300

    Added Register

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad alanı gereklidir.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Kullanıcı Adı alanı gereklidir.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "mail alanı gereklidir.")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "şifre alanı gereklidir.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "şifre alanı gereklidir.")]
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
